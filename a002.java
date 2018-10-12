f667b3370430bb0905c6ab07d0cbf7ef72ce3c17
s17113230 <s17113230@stu.edu.tw>
2018-10-12 14:40:38 +0800

    no message

---
+++ a002.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class a002{
+  public static void main(String [] args){
+    Scanner num=new Scanner(System.in);
+    while(num.hasNext()){
+      int a=num.nextInt(),b=num.nextInt();
+      System.out.println(a+b);
+    }
+  }
+}
